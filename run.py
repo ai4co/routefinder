af7c37b9e4f18392aea7fab9f087e6b17553b2fe
Federico Berto <berto.federico2@gmail.com>
2024-07-19 15:49:53 +0900

    [BugFix] missing model

--- run.py
+++ run.py
@@ -46,7 +46,7 @@ def run(cfg: DictConfig) -> Tuple[dict, dict]:
     callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
 
     log.info("Instantiating loggers...")
-    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
+    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"), model=model)
 
     log.info("Instantiating trainer...")
     trainer: RL4COTrainer = hydra.utils.instantiate(
